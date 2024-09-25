20000c7a558ffc5b78170a1835f6957b2e52e552
jutudey <aj@eriksen.cz>
2024-09-25 13:32:30 +0200

    Day 33 - move list comprenehsion to main. add plot line for Temp

--- backend.py
+++ backend.py
@@ -2,7 +2,7 @@ import requests
 
 API_KEY = "dc8e93233bd017a27a294fc3582b7b48"
 
-def get_data(place,forecast_days, kind):
+def get_data(place, forecast_days):
 
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
@@ -10,12 +10,8 @@ def get_data(place,forecast_days, kind):
     filtered_data = data['list']
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature":
-        filtered_data = [dict["main"]['temp'] for dict in filtered_data]
-    if kind == "Weather Conditions":
-        filtered_data = [dict["weather"][0]['main'] for dict in filtered_data]
 
     return filtered_data
 
 if __name__=="__main__":
-    print(get_data(place="Tokyo", forecast_days=2, kind="Weather Conditions"))
+    print(get_data(place="Tokyo", forecast_days=2))
--- main.py
+++ main.py
@@ -2,19 +2,26 @@ import streamlit as st
 import plotly.express as px
 from backend import get_data
 
+# add GUI elements
 st.title("Weather Forecast for the next days")
-place = st.text_input("Place: ")
+place = st.text_input("Name of city you want the weather forcast for: ")
 days = st.slider('Forecast days: ', min_value=1, max_value=5,
                  help="Select the number of days you want included in the forecast")
-option = st.selectbox('Select data to view:', ("Temperature", "Weather Conditions"))
+option = st.radio('Select data to view:', ("Temperature", "Weather Conditions"))
 
-st.subheader(f"{option} for the next {days} in {place}")
+if place:
+    st.subheader(f"{option} for the next {days} in {place}")
 
-# dates = ['2024-10-09', '2024-10-10', '2024-10-11']
-# temperatures = [10, 11, 15]
-# figure = px.line(x=dates, y=temperatures, labels={'x': "Date", "y":"Temperature in C"})
+    # fetch data via API
+    filtered_data = get_data(place, days)
 
-data = get_data(place, days, option)
+    if option == "Temperature":
+        temperatures = [dict["main"]['temp'] for dict in filtered_data]
+        date = [dict['dt_txt'] for dict in filtered_data]
+        # create temperature plot
+        figure = px.line(x=date, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+        st.plotly_chart(figure)
 
+    if option == "Weather Conditions":
+        filtered_data = [dict[")weather"][0]['main'] for dict in filtered_data]
 
-st.plotly_chart(figure)
